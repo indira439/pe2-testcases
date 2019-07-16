b3624c8822e8e9949d2c846cdd8f4b2df8dc1dd8
indu <indira.indukuri@cgi.com>
2019-07-16 17:37:52 +0530

    ReadFile Test case completed

---
+++ src/test/java/com/stackroute/pe2/ReadFileTest.java
@@ -0,0 +1,63 @@
+package com.stackroute.pe2;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.*;
+
+public class ReadFileTest {
+
+    ReadFile readFile;
+
+    @BeforeClass
+    public void setUp() {
+        readFile = new ReadFile();
+    }
+
+    @After
+    public void tearDown() {
+        readFile = null;
+    }
+
+    @Test
+    public void givenFileNameShouldReadTheContentOfTheFileAndReturnTheContentOfFileInUppercase() {
+        //act
+        String actualResult = readFile.fileReader("sample",".txt");
+        //assert
+        assertEquals("THIS IS A SAMPLE FILE.",actualResult);
+    }
+
+    @Test
+    public void givenFileShouldReturnTheLengthOfTheFile() {
+        //act
+        String actualResult = readFile.lengthOf("sample",".txt");
+        //assert
+        assertEquals(22,actualResult);
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void givenInvalidFileShouldThrowException() {
+        //act
+        readFile.fileReader("SAMple",".txt");
+        readFile.fileReader(" ",".txt");
+    }
+
+    @Test
+    public void givenFileNameShouldReadTheContentOfTheFileAndReturnTheContentOfFileInUppercaseFailure() {
+        //act
+        String actualResult = readFile.fileReader("sample",".txt");
+        //assert
+        assertEquals("THIS IS A SAMPLe FILE.",actualResult);
+    }
+
+    @Test
+    public void givenFileShouldReturnTheLengthOfTheFileFailure() {
+        //act
+        String actualResult = readFile.lengthOf("sample",".txt");
+        //assert
+        assertEquals(12,actualResult);
+    }
+}
